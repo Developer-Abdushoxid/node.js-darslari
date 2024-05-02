b78955cf09b3739b9c848c52d55eb8e0d6119e64
Developer-Abdushoxid <abdushoxidbannayev@gmail.com>
2024-05-02 16:46:12 +0500

    1.1 darslar

---
+++ node.js
@@ -0,0 +1,74 @@
+                // NODE JS DARSLARI
+
+/*
+             Bismillahir rohmanir rohiym
+
+  Nade.js nima - Node js ochiq manbali server muhitidir. Node js turli platformalarda ishlaydi
+   Node js serverda Java script dan foydalanadi node js asixron dasturlashdan foydalanadi
+    veb server uchun umumiy vazifa faylni ochish va kontentni mijozga qaytarish hisoblanadi
+
+  Node jsda modullar - ilovangizda kiritmoqchi bo'lgan funksiyalar to'plami
+    HHTP modulini kiritish uchun require() usulidan foydalaniladi
+    misol:
+    const http = require('http');
+
+
+         Fayl tizimlari
+    Node js fayllar 3 turda yaratiladi
+    1. fs.appenFile() - belgilangan tarkibni faylga qo'shadi  agar yo'q  bo'lsa uni yaratadi
+    2. fs.open() - ikkinchi argument sifatida 'bayroqni' oladi agar bayroq yozish uchun w bo'lsa ko'rsatilgan fayl
+    yozish uchun ochiladi agar yo'q bo'lsa yaratadi
+    3. fs.writeFile() - belgilangan fayl mavjud bo'lsa tarkibni o'zgartiradi agar fayl mavjud bo'lmasa ko'rsatilgan 
+    tarkibni o'z ichiga olgan fayl yaratadi
+
+    Fayllarni yangialsh
+    Fayllarni ikki turda yangilash mumkin
+    1.  fs.appenFile() - belgilangan faylning oxiriga ko'rsatilgan tarkibni qo'shadi
+    2.  fs.writeFile() - belgilangan fayl va tarkibni almashtiradi
+
+    Fayllarni o'chirish
+    Fayllarni fs.unlink () yordamida o'chirishimiz mumkin
+    fs.unlink - belgilangan faylni o'chiradi
+
+    Fayllarni fs.rename() bilan qayta nomlashimiz yani o'zgartirishimiz munkin
+
+    NPM - bu node js paketlari yoki xohlasangiz modullar uchun paket menejer
+    node js o'rnatilganda npm ham o'rnatiladi 
+    paket nima u loyihangizga kiritishingiz uchun kerak bo'lgan barcha fayllarni o'z ichiga oladi
+
+      Node js hodisalari
+    Node js da EventEmitter obyekt yordamida o'zingizni voqealaringizga ishlov berishingiz mumkin 
+    Hodisani yoqish uchun emit() usulidan foydaniladi
+
+    Node jsda fayllarni yuklash uchun Formidable modulidan foydalanamiz
+
+    Node jsda elektron pochta xabarlarini yuborishda Nodemailler modulidan foydalanami
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
